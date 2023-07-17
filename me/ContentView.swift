5b69a76b76f07b51bf9550fbab5d710327d26a9e
Kaavya <kaavya475@gmail.com>
2023-07-17 09:54:50 -0500

    Initial Commit

---
+++ me/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  me
+//
+//  Created by Scholar on 7/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+        @State private var text = ""
+    var body: some View {
+        VStack (spacing:10){
+            Text("Kaavya")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.purple)
+            Image("tennis")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .padding(.horizontal)
+                .font(.largeTitle)
+                .foregroundColor(Color.pink)
+            Button("About me") {
+                text = "I live in Austin, Texas, love to play tennis, and hanging out with my friends"
+                
+            }
+        }
+        .buttonStyle(.bordered)
+        .tint(.pink)
+        Text(text)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ me/meApp.swift
@@ -0,0 +1,17 @@
+//
+//  meApp.swift
+//  me
+//
+//  Created by Scholar on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
